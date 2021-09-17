5cada60279ebd6e9a19ae99591d684ee58ccac45
raniarharistya <90809061+raniarharistya@users.noreply.github.com>
2021-09-17 10:59:06 +0700

    Update DQLabStore-overall-performance.sql

--- Project Data Analysis for Retail: Sales Performance Report/DQLabStore-overall-performance.sql
+++ Project Data Analysis for Retail: Sales Performance Report/DQLabStore-overall-performance.sql
@@ -1,9 +1,27 @@
 -- Overall Performance by Year
 SELECT 
-  year(order_date) as years, sum(sales) as sales, count(order_status) as number_of_order
+  YEAR(order_date) as years, 
+  SUM(sales) as sales, 
+  count(order_status) as number_of_order
 FROM
   dqlab_sales_store
 WHERE
   order_status = 'Order Finished' 
 GROUP BY 
   years; 
+  
+ -- Overall Performance by Product Sub Category
+ SELECT 
+	YEAR(order_date) AS years, 
+	product_sub_category, 
+	SUM(sales) AS sales 
+FROM 
+	dqlab_sales_store 
+WHERE 
+	YEAR(order_date) BETWEEN 2011 AND 2012 AND 
+	order_status = 'Order Finished' 
+GROUP BY 
+	years, product_sub_category
+ORDER BY 
+	years asc, sales desc;
+
